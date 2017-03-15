bbf48d9bb0046b8011fbf149c357526ed907bb42
bjornmulder <bjornmulder.dev@gmail.com>
2017-03-15 07:06:09 +0100

    adds ability to create new todo items
    display modal > take user input && generate todo item id > save to firebase.

--- public/javascripts/todo.js
+++ public/javascripts/todo.js
@@ -1,4 +1,4 @@
-class App {
+class Todo {
   constructor() {
     this.user = JSON.parse(this.readCookie("user"))
     this.config = {
@@ -7,24 +7,24 @@ class App {
       databaseURL: "https://todo-ce199.firebaseio.com",
       storageBucket: "todo-ce199.appspot.com",
       messagingSenderId: "1093069006026"
-    };
-    firebase.initializeApp(this.config);
-    this.database = firebase.database();
+    }
+    firebase.initializeApp(this.config)
+    this.database = firebase.database()
   }
   readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
+    var nameEQ = name + "="
+    var ca = document.cookie.split(';')
     for(var i=0;i < ca.length;i++) {
       var c = ca[i];
-      while (c.charAt(0)==' ') c = c.substring(1,c.length);
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+      while (c.charAt(0)==' ') c = c.substring(1,c.length)
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length)
     }
-    return null;
+    return null
   }
-  genItem(header, body){
+  genItem(header, body, todoID){
     let item = ''
     item += '<div class=\'item\' id=\'item-'
-    item += app.itemCounter
+    item += todo.itemCounter
     item += '\'>'
     item += '<div class=\'itemHeader\'>'
     item += '<h3>'
@@ -37,26 +37,48 @@ class App {
     item += '</p>'
     item += '</div>'
     item += '<div class=\'itemActions text-center\'>'
-    item += '<button class=\'btn btn-danger\' onclick=\'app.deleteItem(this)\'>delete</button>'
-    item += '<button  onclick=\'app.getItemData(this);\' class=\'btn btn-primary\'>edit</button>'
+    item += '<button class=\'btn btn-danger\' onclick=\'todo.deleteItem(this)\'>delete</button>'
+    item += '<button  onclick=\'todo.getItemData(this)\' class=\'btn btn-primary\'>edit</button>'
+    item += '<input type="hidden" name="todoID" id="todoID" value="'+ todoID + '">'
     item += '</div>'
     item += '</div>'
-    app.itemCounter ++
+    todo.itemCounter ++
     // append item to list
     $('#list').append(item)
   }
-  writeUserData() {
-    firebase.database().ref('lists/' + app.user.uid).set({
-      username: app.user.email,
-      todoItem: "test"
-    });
+  writeUserData(header, body, todoID) {
+    firebase.database().ref('lists/' + todo.user.uid + '/' + todoID).set({
+      username: todo.user.email,
+      todoItem: todoID,
+      todoHeader: header,
+      todoBody: body
+    })
   }
-    getUserData(){
-    var userId = firebase.auth().currentUser.uid;
-    return firebase.database().ref('/lists/' + app.user.uid).once('value').then(function(snapshot) {
-      var displayname = snapshot.val().username;
-    });
-    }
+  getUserData(){
+    var userId = firebase.auth().currentUser.uid
+    return firebase.database().ref('/lists/' + todo.user.uid).once('value').then(function(snapshot) {
+      var displayname = snapshot.val().username
+    })
+  }
+  fetchTodoItems(){
+    var userId = firebase.auth().currentUser.uid
+    return firebase.database().ref('/lists/' + todo.user.uid).once('value').then(function(snapshot) {
+      var items = snapshot.val()
+      console.log(items);
+      for (var i = 0, len = items.length; i < len; i++) {
+        console.log(items[i])
+      }
+    })
+  }
+  // start of user actions
+  newTodo(){
+    let header  = $('#createHeader').val()
+    let body    = $('#createContent').val()
+    let todoID  = "todo-" + Math.random().toString(36).substr(2, 9)
+    todo.writeUserData(header, body, todoID)
+    todo.genItem(header, body, todoID)
+    $('#createModal').modal('hide')
+  }
 }
-var app = new App()
+var todo = new Todo()
 
